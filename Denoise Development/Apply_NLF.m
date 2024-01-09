3129775414e05163e30996d299ec3769cbef7260
ziwzh166 <ziwen.zhao@kemi.uu.se>
2024-01-09 19:53:09 +0100

    Updating the fitting

--- Denoise Development/Apply_NLF.m
+++ Denoise Development/Apply_NLF.m
@@ -1,46 +1,51 @@
-%% 2023-10-09 Mohammad: Apply non-linear filter
-%       Used in Find_Steps
-%       MATLAB 2023a
-% data is a vector contains the output of a single electrode
-% based on what Zinaida-Cecilia wrote
-% This function set N first and N last datapoints to zero, that data will be lost
+% This function applies a non-linear filter to the input data.
+% The filter is based on forward and backward moving averages and a weighting function.
+% The function sets the first N and last N data points to zero.
 
 function Denoised_Data = Apply_NLF (data,N,M,p)
 
+% Get the length of the data
 datalength = length(data);
+% Define the start and finish indices for the main part of the data
 start = N + 1;
 finish = datalength - N;
 
+% If is the forward signal
+% Initialize with zeros
 If = zeros(datalength,1);
+
+% Ib is the backward signal
+% Initialize with zeros
 Ib = zeros(datalength,1);
-    
-%points intensities
+
+% Calculate the forward and backward moving averages
 for i = start:finish
-    If(i) = mean(data(i-N:i-1));
-    Ib(i) = mean(data(i+1:i+N));  
+    If(i) = mean(data(i-N:i-1));  % Forward moving average
+    Ib(i) = mean(data(i+1:i+N));  % Backward moving average
 end
 
+% Initialize the forward and backward weights with ones
 forward = ones(datalength, 1);
 backward = ones(datalength, 1);
 
+% Initialize the summation variables for the weight calculation
 summF = 0;
 summB = 0;
 
-%weight functions
+% Calculate the weights based on the difference between the data and the moving averages
 for i = start:finish
-
     for j = 0 : M-1
+        % Accumulate the squared differences for the forward and backward weights
         summF = summF + ( data(i-j) - If(i-j) )^2 ;
         summB = summB + ( data(i-j) - Ib(i-j) )^2 ;
     end
     
+    % Calculate the forward and backward weights
     forward(i) = 1/(1+ (summF/summB)^p );
     backward(i) = 1/(1+ (summB/summF)^p );
-    
 end
 
-%final signals
+% Combine the forward and backward signals using the weights to get the final denoised data
 Denoised_Data =  forward.* If + backward.* Ib;
 
 end
-
--- Denoise Development/Test.m
+++ Denoise Development/Test.m
@@ -1,16 +1,144 @@
-%% simulate some step looking signals with guassian noise plus the decreasing trend
 
-% Step length
-LenthData = 1e5;
+%% Simulate data
+[x,y,Random_step_idx,Random_spike_idx] = simData(1e5);
+y_denoised = Apply_NLF(y,40,20,10);
+%% set a height thresold and length threshold for step
+step_thresold = 0.4;
+Step_length = 20;
+stepIni = y_denoised(Random_step_idx-Step_length);
+StepEnd = y_denoised(Random_step_idx+Step_length);
+StepLen = x(Random_step_idx+Step_length) - x(Random_step_idx-Step_length);
+step =abs(StepEnd - stepIni);
+step_idx = find(step>step_thresold);
+step_idx = step_idx(StepLen(step_idx)>Step_length);
+Random_step_idx_filtered = Random_step_idx(step_idx);
+%% 
+%% Plot data and the index of step and spike
+figure;
+t = tiledlayout(2,1)
+ax(1) = nexttile;
+plot(x,y)
+hold on;
+plot(x(Random_step_idx_filtered),y(Random_step_idx_filtered),'*','DisplayName','step')
+ax(2) = nexttile;
+plot(x,y_denoised)
+hold on;
+plot(x(Random_step_idx_filtered),y_denoised(Random_step_idx_filtered),'*','DisplayName','step')
+linkaxes(ax,'x')
+xlabel(t,'x')
+ylabel(t,'y')
+legend show
+%% Check prime of Denoise signal with diff
+ydiff = diff(y_denoised);
+figure;
+tiledlayout(2,1)
+ax(1) = nexttile;
+% plot(x(1:end-1),diff(y))
+hold on;
+plot(x(1:end-1),ydiff,'*','DisplayName','step')
+plot(x(Random_step_idx_filtered),ydiff(Random_step_idx_filtered),'*','DisplayName','step')
+ax(2) = nexttile;
+plot(x,y)
+hold on;
+plot(x,y_denoised)
+plot(x(Random_step_idx_filtered),y_denoised(Random_step_idx_filtered),'|','MarkerSize',20,'DisplayName','step','LineWidth',5)
+linkaxes(ax,'x')
+%% Prepare the training set
+%Length of the stride
+StrideLen = 1;
+%Number of clips
+NumClip = 1 + floor((length(y) - Step_length) / StrideLen);
+X = zeros(Step_length,NumClip);
+y_Class = zeros(1,NumClip);
+for i = 1:NumClip
+    try
+        X(:,i) = y_denoised(i*StrideLen:i*StrideLen+Step_length-1);
+    catch
+        X(:,i) = y_denoised(i*StrideLen:end);
+    end
+    if ismember(i*StrideLen,Random_step_idx_filtered-Step_length/2)
+        y_Class(i) = 1;
+    end
+end
+%% Train the model
+% SVM model
+SVMModel = fitcsvm(X',y_Class,'KernelFunction','rbf','Standardize',true,'ClassNames',[0,1]);
+%% 
+% LSTM model
+layers = [ ...
+    sequenceInputLayer(Step_length)
+    lstmLayer(100,'OutputMode','sequence')
+    fullyConnectedLayer(2)
+    tanhLayer
+    regressionLayer];
+options = trainingOptions('adam', ...
+    'MaxEpochs',100, ...
+    'GradientThreshold',1, ...
+    'InitialLearnRate',0.01, ...
+    'LearnRateSchedule','piecewise', ...
+    'LearnRateDropPeriod',50, ...
+    'LearnRateDropFactor',0.2, ...
+    'Verbose',0, ...
+    'Plots','training-progress');
+y_0 = y_Class == 0;
+y_train = [y_Class;y_0];
+X_norm = normalize(X);
+LSTMModel = trainNetwork(X_norm,y_train,layers,options);
+
+
+%% Test the model
+y_pred_SVM = predict(SVMModel,X');
+y_pred_LSTM = predict(LSTMModel,X);
+y_pred_LSTM(y_pred_LSTM < 0.5) = 0;
+y_pred_LSTM(y_pred_LSTM >= 0.5) = 1;
+%% plot the confusion matrix
+figure;
+y_SVM0 = y_pred_SVM == 0;
+plotconfusion(y_train,[y_pred_SVM';y_SVM0'])
+figure;
+plotconfusion(y_train,y_pred_LSTM)
+%% Plot on the graph
+figure;
+tiledlayout(2,1)
+ax(1) = nexttile;
+plot(x,y)
+hold on;
+plot(x(Random_step_idx_filtered),y(Random_step_idx_filtered),'*','DisplayName','step')
+for i = 1:length(y_pred_SVM)
+    if y_pred_SVM(i) == 1
+        idx = i*StrideLen:i*StrideLen+Step_length-1;
+        fprintf('step idx: %d\n',idx(1))
+        plot(X,y(i*StrideLen:i*StrideLen+Step_length-1),'|')
+    end
+end
+legend show
+ax(2) = nexttile;
+plot(x,y)
+hold on;
+plot(x(Random_step_idx_filtered),y_denoised(Random_step_idx_filtered),'*','DisplayName','step')
+for i = 1:length(y_pred_LSTM)
+    if y_pred_LSTM(i) == 1
+        plot(x(i*StrideLen:i*StrideLen+Step_length-1),y_denoised(i*StrideLen:i*StrideLen+Step_length-1),'r')
+    end
+end
+linkaxes(ax,'x')
+
+
+%% function section
+function [x,y,Random_step_idx,Random_spike_idx] = simData(LenthData)
+% Simulate data
+if nargin == 0
+    % default length of data
+    LenthData = 1e5;
+end
 % x axis
 x = 1:LenthData;
 % y axis
 y = zeros(1,LenthData);
-% random step index
-Random_step_idx = sort(randi(LenthData,1,LenthData/1e3));
-% random index for adding spikes
-Random_spike_idx = sort(randi(LenthData,1,LenthData/1e4));
-% initial step value
+% random step idx
+Random_step_idx = randi([1,LenthData],1,100);
+% random spike idx
+Random_spike_idx = randi([1,LenthData],1,100);
 num = 10;
 
 i = 1;
@@ -33,44 +161,7 @@ while i <=LenthData
         i = i + 1;
     end
 end
-
-fprintf('Wavelet denoised\n')
-tic
-y_denoised = wdenoise(y,4);
-toc
-fprintf('Non-linear filter applied\n')
-tic
-y_filter= Apply_NLF(y,10,5,10);
-toc
-fprintf('FIR linear filter applied\n')
-tic
-y_fir = lowpass(y,0.03);
-toc
-
-
-% plot
-figure;
-plot(x,y,'LineWidth',2,'DisplayName','Simulated');
-hold on;
-plot(x,y_denoised,'DisplayName','Wave - Denoised','LineWidth',2);
-plot(x,y_filter,'DisplayName','Non-linear- Filtered','LineWidth',2);
-plot(x,y_fir,'DisplayName','FIR - Filtered','LineWidth',2);
-xlabel('x')
-ylabel('y')
-legend show
-%% Check prime of Denoise signal with diff
-figure;
-tiledlayout(2,1)
-ax(1) = nexttile;
-% plot(x(1:end-1),diff(y))
-hold on;
-plot(x(1:end-1),diff(y_denoised))
-ax(2) = nexttile;
-plot(x,y)
-hold on;
-plot(x,y_denoised)
-linkaxes(ax,'x')
-
+end
 
 
 
